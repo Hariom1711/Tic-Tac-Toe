d8ef85ed034a7f7ffe38aa278c566af7ad963983
Hariom ojha <97445797+Hariom1711@users.noreply.github.com>
2022-04-28 02:11:50 +0530

    changes

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -2,11 +2,13 @@ import React from "react"
 
 
 function SquareComponents(props) {
- const classes =props.className ?`${props.className} square`:'square'
-  return (
-   <span className={classes} onClick={props.onClick}>
-     {props.state}
-   </span>
+ const classes = (props.className ? `${props.className} square` : `square`)
+    return (
+        <span
+            className={classes + (props.state === "X" ? ` fc-aqua` : ` fc-white`)}
+            onClick={() => props.onClick(props.index)}>
+           {props.state}
+        </span>
    
   )
 }
